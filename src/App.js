6897bf5764bfea662f2a9632c9e4950c21d5dc48
BAssal <baslm8496@gmail.com>
2024-03-22 13:59:22 +0200

    chancing some files name and adding some comments

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import { Col, Container, Row } from "react-bootstrap";
 import TitleProject from "./components/TitleProject";
 import FormInput from "./components/FormInput";
 import AccordionQAndA from "./components/AccordionQAndA";
-import { question } from "./api/Qusetoin";
+import { question } from "./api/Question";
 
 function App() {
-  const [data, setdata] = useState(question);
+  const [data, setData] = useState(question);
+  // *  add item to list of questions
+  const addItem = () => {
+    setData([...question]);
+  };
+  // * deleted All data
+  const deletedAllData = () => {
+    question.splice(0, question.length);
+    setData([]);
+  };
 
-  const addItem =()=>{
-    setdata([...question])
-
-  }
-
-  const deletedAllData=()=>{
-    question.splice(0,question.length)
-    setdata([])
-  }
+  const deletedOneItem = (items) => [setData([...items])];
   return (
     <div className="font text-center color-body">
       <Container className="p-5">
@@ -26,10 +27,12 @@ function App() {
           </Col>
           <Col md="8">
             <FormInput addData={addItem} />
-            <AccordionQAndA data={data} />
+            <AccordionQAndA data={data} deletedOneItem={deletedOneItem} />
 
             {data.length >= 1 ? (
-              <button onClick={deletedAllData} className="w-100 btn-color">مسح الكل</button>
+              <button onClick={deletedAllData} className="w-100 btn-color">
+                مسح الكل
+              </button>
             ) : null}
           </Col>
         </Row>
